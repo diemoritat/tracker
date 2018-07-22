4f350f593d46b52070d69b9052830f2a59546e96
Nayara Alves <nayara.nap@gmail.com>
2018-07-22 01:30:53 -0300

    Update form validation. Migrate from Vuelidate to vee-validate

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate from 'vee-validate'
 
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   beforeCreate() {
-		this.$store.commit('initialiseStore');
+		this.$store.commit('initialiseStore')
 	},
   render: h => h(App)
 }).$mount('#app')
